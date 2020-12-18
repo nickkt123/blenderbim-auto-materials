ca99d5dbd698412936c1c478ab64375a0df4d037
Nick Kleine-Tebbe <nick@kleine-tebbe.de>
2020-12-18 14:51:06 +0100

    Enable Transparency / Highlight Missing materials

--- __init__.py
+++ __init__.py
@@ -1,26 +1,50 @@
+import bpy
+
 bl_info = {
     "name": "BlenderBIM Auto-materials",
     "blender": (2, 80, 0),
     "category": "Object",
 }
 
-import bpy
-
 
 class ObjectMoveX(bpy.types.Operator):
-    """My Object Moving Script"""      # Use this as a tooltip for menu items and buttons.
+    """Convert BIM Materials to Cycles Materials."""      # Use this as a tooltip for menu items and buttons.
+
     bl_idname = "object.move_x"        # Unique identifier for buttons and menu items to reference.
     bl_label = "BlenderBIM Auto-materials"         # Display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
 
     def execute(self, context):        # execute() is called when running the operator.
 
-        # The original script
-        scene = context.scene
-        for obj in scene.objects:
-            obj.location.x += 1.0
+        materials = bpy.data.materials
+        for material in materials:
+            diffuse_color = material.diffuse_color
 
+            material.use_nodes = True
+            node = material.node_tree.nodes.get("Principled BSDF")
+            node.inputs['Base Color'].default_value = (
+                diffuse_color[0],
+                diffuse_color[1],
+                diffuse_color[2],
+                diffuse_color[3]
+            )
+            transparency = diffuse_color[3] < 1.0
+            if transparency:
+                material.use_screen_refraction = True
+                node.inputs['Roughness'].default_value = 0
+                node.inputs['Transmission'].default_value = 1 - diffuse_color[3]
+
+        empty_mat = bpy.data.materials.new('Empty Material')
+        empty_mat.use_nodes = True
+        node = empty_mat.node_tree.nodes.get("Principled BSDF")
+        node.inputs['Base Color'].default_value = (1, 0, 1, 1)
+
+        for obj in bpy.context.selected_objects:
+            if obj.type == 'MESH':
+                materials = obj.material_slots
+                obj.data.materials.append(empty_mat)
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+
 
 def register():
     bpy.utils.register_class(ObjectMoveX)
